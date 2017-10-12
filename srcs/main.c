14de1d97fcac0305428e783af00c19d0dfab440d
Julien THILLARD <jthillar@e3r4p22.42.fr>
2017-10-12 17:48:31 +0200

    modif tony2

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jthillar <jthillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/14 11:51:05 by jthillar          #+#    #+#             */
-/*   Updated: 2017/10/12 16:06:35 by jthillar         ###   ########.fr       */
+/*   Updated: 2017/10/12 17:47:37 by jthillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		end_lem_in(t_room *room, t_startend se)
 		return (algo(&room, se));
 }
 
-void	end_line(char *line, t_startend *se, t_room *room)
+int	end_line(char *line, t_startend *se, t_room *room)
 {
 	if (se->com == 0 && se->ants >= 0 && ft_parse_sel(line, se) == 2
 	&& ft_p_room(line) == -1 && se->antsgiven == 0)
@@ -32,9 +32,15 @@ void	end_line(char *line, t_startend *se, t_room *room)
 		if (check_link(se, line, room) == 2)
 			ft_putendl(line);
 	}
+	if (se->n_link > 1 && ft_p_room(line) > 0)
+	{
+		ft_printf("%s : Error : wrong link -> Stop reading map\n", line);
+		return (0);
+	}
 	se->com = 0;
 	se->antsgiven = 0;
 	ft_memdel((void **)&line);
+	return (1);
 }
 
 int		main(void)
@@ -50,10 +56,8 @@ int		main(void)
 		if (first_parse(line, &se) == 0)
 			return (0);
 		if (COM && ANT && PARSE && se.n_link == 0)
-		{
 			if (parse_and_add_room(line, room, &se) == 0)
 				return (0);
-		}
 		if (COM && ANT && PARSE && LOOKLINK && se.antsgiven == 0)
 		{
 			if (check_link(&se, line, room) == 0)
@@ -61,7 +65,8 @@ int		main(void)
 			if (check_link(&se, line, room) == 1)
 				break ;
 		}
-		end_line(line, &se, room);
+		if (end_line(line, &se, room) == 0)
+			break;
 	}
 	return (end_lem_in(room, se));
 }
